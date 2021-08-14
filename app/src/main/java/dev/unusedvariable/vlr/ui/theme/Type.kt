2a2a1a0c0e857edee7e80c4009e85965c0c5ff48
Shreyansh Lodha <slodha96@gmail.com>
2021-08-14 13:01:38 +0530

    Update fonts
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/unusedvariable/vlr/ui/theme/Type.kt
+++ app/src/main/java/dev/unusedvariable/vlr/ui/theme/Type.kt
@@ -5,12 +5,14 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.sp
 import dev.unusedvariable.vlr.R
 
-val regular = Font(R.font.iosevka_light)
-val medium = Font(R.font.iosevka_medium, FontWeight.W500)
-val bold = Font(R.font.iosevka_bold, FontWeight.W600)
+val regular = Font(R.font.exo_light, FontWeight.Light)
+val medium = Font(R.font.exo_regular, FontWeight.Normal)
+val bold = Font(R.font.exo_semibold, FontWeight.SemiBold)
 
 val appFontFamily = FontFamily(
     fonts = listOf(
@@ -24,7 +26,7 @@ val Typography = Typography(
     h4 = TextStyle(
         fontFamily = appFontFamily,
         fontWeight = FontWeight.W600,
-        fontSize = 30.sp
+        fontSize = 30.sp,
     ),
     h5 = TextStyle(
         fontFamily = appFontFamily,
