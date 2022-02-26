c0c3a17522fb4c47b00afd69bf9e61db018bbdad
Shreyansh Lodha <slodha96@gmail.com>
2022-02-27 01:25:30 +0530

    theme: Update Type to use new font
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/unusedvariable/vlr/ui/theme/Type.kt
+++ app/src/main/java/dev/unusedvariable/vlr/ui/theme/Type.kt
@@ -8,9 +8,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import dev.unusedvariable.vlr.R
 
-val regular = Font(R.font.exo_light, FontWeight.Light)
-val medium = Font(R.font.exo_regular, FontWeight.Normal)
-val bold = Font(R.font.exo_semibold, FontWeight.SemiBold)
+val regular = Font(R.font.aile_light, FontWeight.Light)
+val medium = Font(R.font.aile_regular, FontWeight.Normal)
+val bold = Font(R.font.aile_semibold, FontWeight.SemiBold)
 
 val appFontFamily = FontFamily(fonts = listOf(regular, medium, bold))
 
