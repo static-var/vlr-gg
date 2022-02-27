3f246f015e922c70c024dc3102b44660d2c7b485
Shreyansh Lodha <slodha96@gmail.com>
2022-02-28 03:38:17 +0530

    theme: Use Android 12 splash screen API
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/unusedvariable/vlr/MainActivity.kt
+++ app/src/main/java/dev/unusedvariable/vlr/MainActivity.kt
@@ -3,6 +3,7 @@ package dev.unusedvariable.vlr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.google.accompanist.insets.ProvideWindowInsets
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dev.unusedvariable.vlr.ui.theme.VLRTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
     WindowCompat.setDecorFitsSystemWindows(window, false)
     setContent { VLRTheme() { ProvideWindowInsets { VLR() } } }
