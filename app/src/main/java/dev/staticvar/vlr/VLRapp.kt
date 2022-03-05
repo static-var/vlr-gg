df920de2a0a3dc4804e2bd4a0f26c2e885febdbb
Shreyansh Lodha <slodha96@gmail.com>
2022-03-05 17:04:34 +0530

    `VLRApp`: seprate out Firebase Init
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/VLRapp.kt
+++ app/src/main/java/dev/staticvar/vlr/VLRapp.kt
@@ -16,25 +16,28 @@ class VLRapp() : Application() {
   override fun onCreate() {
     super.onCreate()
     Logger.init(true)
+    firebaseInit()
+    createNotificationChannel()
+  }
 
+  private fun firebaseInit() {
     FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
-      if(task.isSuccessful) {
-        i {"FCM Token ${task.result}"}
+      if (task.isSuccessful) {
+        i { "FCM Token ${task.result}" }
       } else {
-        e {"FCM Token error"}
+        e { "FCM Token error" }
       }
     }
-
-    createNotificationChannel()
   }
 
   private fun createNotificationChannel() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val channel = NotificationChannel(getString(R.string.notification_channel_id),
-        getString(R.string.notification_channel_title),
-        NotificationManager.IMPORTANCE_HIGH)
+      val channel =
+          NotificationChannel(
+              getString(R.string.notification_channel_id),
+              getString(R.string.notification_channel_title),
+              NotificationManager.IMPORTANCE_HIGH)
       getSystemService<NotificationManager>()?.createNotificationChannel(channel)
     }
-
   }
 }
