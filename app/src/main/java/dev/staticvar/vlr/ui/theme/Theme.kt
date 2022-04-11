747b1570a02fc12476dfca4e5cd8e02c6fa11a89
Shreyansh Lodha <slodha96@gmail.com>
2022-04-11 23:26:47 +0530

    `Theme`: Add theme for Glance Widget
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/ui/theme/Theme.kt
+++ app/src/main/java/dev/staticvar/vlr/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package dev.staticvar.vlr.ui.theme
 
+import android.content.Context
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
@@ -27,6 +28,31 @@ fun VLRTheme(
     when {
       dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
         val context = LocalContext.current
+        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      }
+      darkTheme -> DarkColorScheme
+      else -> LightColorScheme
+    }
+
+  MaterialTheme(
+    colorScheme = colorScheme,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
+}
+
+
+@Composable
+fun WidgetTheme(
+  context: Context,
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  dynamicColor: Boolean = true,
+  content: @Composable () -> Unit
+) {
+  val colorScheme =
+    when {
+      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
         if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
       }
       darkTheme -> DarkColorScheme
