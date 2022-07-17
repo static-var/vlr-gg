428fd1850fc92bfdfded2ca606fded9b893e6f87
Shreyansh Lodha <slodha96@gmail.com>
2022-07-17 17:36:22 +0530

    app: Add baseline profile
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
@@ -2,28 +2,14 @@ package com.example.benchmark
 
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
-import androidx.test.uiautomator.By
 import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalBaselineProfilesApi
 class BaselineProfileGenerator {
-  @get:Rule
-  val baselineProfileRule = BaselineProfileRule()
+  @get:Rule val baselineProfileRule = BaselineProfileRule()
 
   @Test
   fun startup() =
-    baselineProfileRule.collectBaselineProfile(
-      packageName = "dev.staticvar.vlr"
-    ) {
-      pressHome()
-      startActivityAndWait()
-      device.waitForIdle()
-
-      device.findObject(By.text("Matches")).click()
-      device.waitForIdle()
-
-      device.findObject(By.text("Events")).click()
-      device.waitForIdle()
-    }
+    baselineProfileRule.collectBaselineProfile(packageName = "dev.staticvar.vlr") { userflow() }
 }
