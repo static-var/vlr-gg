6f6119b96595fd3491b549be771a127f58b4b069
Shreyansh Lodha <slodha96@gmail.com>
2022-08-02 17:45:29 +0530

    app: Remove all related to app update and change log section
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/VlrFileProvider.kt
+++
@@ -1,6 +0,0 @@
-package dev.staticvar.vlr
-
-import androidx.annotation.XmlRes
-import androidx.core.content.FileProvider
-
-class VlrFileProvider(@XmlRes val resourceId: Int = R.xml.file_path) : FileProvider(resourceId)
