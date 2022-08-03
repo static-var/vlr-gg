feb6343ec3d19ebe4289781238cb5db7a348cf34
Shreyansh Lodha <slodha96@gmail.com>
2022-08-03 10:29:08 +0530

    `About`: Add sponsor button and remove release button
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/utils/Endpoints.kt
+++ app/src/main/java/dev/staticvar/vlr/utils/Endpoints.kt
@@ -8,12 +8,6 @@ object Endpoints {
   private const val EVENT_DETAILS = "api/v1/events/"
   private const val MATCH_DETAILS = "api/v1/matches/"
 
-  const val APK_VERSION_PAGE_LINK =
-    "https://raw.githubusercontent.com/static-var/vlr-gg/trunk/version"
-  const val CHANGFELOG_PAGE_LINK =
-    "https://raw.githubusercontent.com/static-var/vlr-gg/trunk/CHANGELOG.md"
-  const val APK_DOWNLOAD_PAGE_LINK = "https://github.com/static-var/vlr-gg/releases/latest"
-
   fun teamDetails(id: String) = TEAM_DETAILS + id
   fun eventDetails(id: String) = EVENT_DETAILS + id
   fun matchDetails(id: String) = MATCH_DETAILS + id
