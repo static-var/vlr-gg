b7a7164f55177a112cd805affc251b90ba1aadca
Shreyansh Lodha <slodha96@gmail.com>
2022-08-16 00:41:57 +0530

    `DB`: Add migration from version 7 to 8
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/data/db/VlrDB.kt
+++ app/src/main/java/dev/staticvar/vlr/data/db/VlrDB.kt
@@ -3,6 +3,8 @@ package dev.staticvar.vlr.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dev.staticvar.vlr.data.api.response.*
 import dev.staticvar.vlr.data.dao.VlrDao
 import dev.staticvar.vlr.data.model.TopicTracker
@@ -19,17 +21,32 @@ import dev.staticvar.vlr.data.model.TopicTracker
       TeamDetails::class,
     ],
   exportSchema = false,
-  version = 7
+  version = 8
 )
 @TypeConverters(VlrTypeConverter::class)
 abstract class VlrDB : RoomDatabase() {
   abstract fun getVlrDao(): VlrDao
 }
 
+val Migration_7_8 =
+  object : Migration(7, 8) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+      val time = System.currentTimeMillis().toString()
+      database.execSQL("ALTER TABLE MatchInfo ADD COLUMN createdAt TEXT NOT NULL DEFAULT ''")
+      database.execSQL("ALTER TABLE TeamDetails ADD COLUMN createdAt TEXT NOT NULL DEFAULT ''")
+      database.execSQL("ALTER TABLE TournamentDetails ADD COLUMN createdAt TEXT NOT NULL DEFAULT ''")
+
+      database.execSQL("UPDATE MatchInfo set createdAt = $time where createdAt = ''")
+      database.execSQL("UPDATE TeamDetails set createdAt = $time where createdAt = ''")
+      database.execSQL("UPDATE TournamentDetails set createdAt = $time where createdAt = ''")
+    }
+  }
+
 /**
  * Version change log 3 -> Remove old tables and create new for accommodating API calls
  *
- * 4 -> Add FCM tracker table 5 -> Add more stats to players in match
+ * 4 -> Add FCM tracker table
+ * 5 -> Add more stats to players in match
  * 6 -> Add status in events for MatchDetailsScreen
  * 7 -> Add rounds information for every match
  */
