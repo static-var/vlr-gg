df78277ed207f0f7e35d898436962352e25e3928
Shreyansh Lodha <slodha96@gmail.com>
2022-08-26 19:17:03 +0530

    VlrNavBar.kt: Fix Nav bar not being positioned correctly

--- app/src/main/java/dev/staticvar/vlr/ui/helper/VlrNavBar.kt
+++ app/src/main/java/dev/staticvar/vlr/ui/helper/VlrNavBar.kt
@@ -1,7 +1,8 @@
 package dev.staticvar.vlr.ui.helper
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -13,26 +14,31 @@ import androidx.navigation.NavHostController
 import dev.staticvar.vlr.ui.theme.VLRTheme
 
 @Composable
-fun NewNavBar(navController: NavHostController, items: List<NavItem>, currentNav: String) {
-  AnimatedVisibility(
-    visible = items.any { it.route == (navController.currentDestination?.route ?: "") }
-  ) {
-    NavigationBar(tonalElevation = 8.dp, modifier = Modifier.navigationBarsPadding()) {
-      items.forEach { navItem ->
-        NavigationBarItem(
-          selected = navController.currentDestination?.route == navItem.route,
-          icon = {
-            Crossfade(currentNav == navItem.route) {
-              Icon(
-                imageVector = if (it) navItem.selectedIcon else navItem.unselectedIcon,
-                contentDescription = navItem.title,
-                tint = VLRTheme.colorScheme.onPrimaryContainer
-              )
-            }
-          },
-          label = { Text(text = navItem.title) },
-          onClick = navItem.onClick
-        )
+fun VlrNavBar(navController: NavHostController, items: List<NavItem>, currentNav: String) {
+
+  // https://issuetracker.google.com/issues/243852341#comment1
+  Box(modifier = Modifier.animateContentSize()) {
+    if (items.any { it.route == (navController.currentDestination?.route ?: "") }) {
+      NavigationBar(
+        tonalElevation = 8.dp,
+        modifier = Modifier.navigationBarsPadding().animateContentSize()
+      ) {
+        items.forEach { navItem ->
+          NavigationBarItem(
+            selected = navController.currentDestination?.route == navItem.route,
+            icon = {
+              Crossfade(currentNav == navItem.route) {
+                Icon(
+                  imageVector = if (it) navItem.selectedIcon else navItem.unselectedIcon,
+                  contentDescription = navItem.title,
+                  tint = VLRTheme.colorScheme.onPrimaryContainer
+                )
+              }
+            },
+            label = { Text(text = navItem.title) },
+            onClick = navItem.onClick
+          )
+        }
       }
     }
   }
