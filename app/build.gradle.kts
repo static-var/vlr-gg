33deda2c9a9e47bbcf589605dddf677dcba89dcd
Shreyansh Lodha <slodha96@gmail.com>
2022-08-30 19:31:42 +0530

    `gradle`: Prepare for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
     applicationId = "dev.staticvar.vlr"
     minSdk = 23
     targetSdk = 33
-    versionCode = 39
-    versionName = "v0.2.13"
+    versionCode = 40
+    versionName = "v0.2.14"
 
     setProperty("archivesBaseName", "${applicationId}-${versionCode}(${versionName})")
   }
