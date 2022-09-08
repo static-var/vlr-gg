28e4e35236654b3daacb1f64c60eddaa60dd332a
Shreyansh Lodha <slodha96@gmail.com>
2022-09-08 10:52:51 +0530

    `Endpoint`: Prepare to integrate Player API

--- app/src/main/java/dev/staticvar/vlr/utils/Endpoints.kt
+++ app/src/main/java/dev/staticvar/vlr/utils/Endpoints.kt
@@ -8,8 +8,10 @@ object Endpoints {
   private const val TEAM_DETAILS = "api/v1/team/"
   private const val EVENT_DETAILS = "api/v1/events/"
   private const val MATCH_DETAILS = "api/v1/matches/"
+  private const val PLAYER_DETAILS = "/api/v1/player/"
 
   fun teamDetails(id: String) = TEAM_DETAILS + id
   fun eventDetails(id: String) = EVENT_DETAILS + id
   fun matchDetails(id: String) = MATCH_DETAILS + id
+  fun playerDetails(id: String) = PLAYER_DETAILS + id
 }
