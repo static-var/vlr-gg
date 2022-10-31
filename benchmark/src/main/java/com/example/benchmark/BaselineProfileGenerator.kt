6d246d23a2034e7b97857d2fccf46e12f4338420
Shreyansh Lodha <slodha96@gmail.com>
2022-11-01 00:26:04 +0530

    gradle: Update dependencies
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/com/example/benchmark/BaselineProfileGenerator.kt
@@ -1,11 +1,9 @@
 package com.example.benchmark
 
-import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import org.junit.Rule
 import org.junit.Test
 
-@ExperimentalBaselineProfilesApi
 class BaselineProfileGenerator {
   @get:Rule val baselineProfileRule = BaselineProfileRule()
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,20 +9,20 @@ plugins {
 }
 
 buildscript {
-  val hilt_version = "2.43.2"
-  val kotlin_version = "1.7.10"
+  val hilt_version = "2.44"
+  val kotlin_version = "1.7.20"
   repositories {
     google()
     mavenCentral()
   }
 
   dependencies {
-    classpath("com.android.tools.build:gradle:7.4.0-alpha10")
+    classpath("com.android.tools.build:gradle:7.3.1")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlin_version")
     classpath("com.google.dagger:hilt-android-gradle-plugin:$hilt_version")
-    classpath("com.google.gms:google-services:4.3.13")
-    classpath("com.google.firebase:perf-plugin:1.4.1")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
+    classpath("com.google.gms:google-services:4.3.14")
+    classpath("com.google.firebase:perf-plugin:1.4.2")
+    classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.2")
   }
 }
