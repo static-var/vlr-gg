ca840b4a424c60a110150ee881ecadc96bc5a865
Shreyansh Lodha <slodha96@gmail.com>
2023-09-02 00:09:47 +0530

    ref(nav): Fix nav visibility

--- app/src/main/java/dev/staticvar/vlr/MainActivity.kt
+++ app/src/main/java/dev/staticvar/vlr/MainActivity.kt
@@ -3,8 +3,8 @@ package dev.staticvar.vlr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 import dev.staticvar.vlr.ui.PaddingLocalCompositions
 import dev.staticvar.vlr.ui.VLR
@@ -13,9 +13,9 @@ import dev.staticvar.vlr.ui.theme.VLRTheme
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
     installSplashScreen()
     super.onCreate(savedInstanceState)
-    WindowCompat.setDecorFitsSystemWindows(window, false)
     setContent { VLRTheme() { PaddingLocalCompositions { VLR() } } }
   }
 }
