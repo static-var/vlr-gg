39d1f69b35a414831b2615239ddf1eabba9aca00
Shreyansh Lodha <slodha96@gmail.com>
2023-10-02 12:54:13 +0530

    gradle(ktfmt): Create Convention plugin
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

---
+++ build-logic/convention/src/main/kotlin/KtfmtConventionPlugin.kt
@@ -0,0 +1,26 @@
+
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.getByType
+
+class KtfmtConventionPlugin : Plugin<Project> {
+
+  companion object {
+    private const val KTFMT_VERSION = "0.46"
+  }
+
+  override fun apply(project: Project) {
+    with(project) {
+      val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+      pluginManager.apply(libs.findPlugin("spotless-plugin").get().get().pluginId)
+      extensions.getByType<SpotlessExtension>().apply {
+        kotlin { ktfmt(KTFMT_VERSION).googleStyle() }
+        kotlinGradle { ktfmt(KTFMT_VERSION).googleStyle() }
+      }
+
+    }
+  }
+}
