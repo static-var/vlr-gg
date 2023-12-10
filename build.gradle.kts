2bf2811bd68a8f09e7ebb0ca027375731154f2dc
Shreyansh Lodha <slodha96@gmail.com>
2023-12-10 14:18:17 +0530

    gradle: Update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,3 +34,22 @@ buildscript {
     classpath(libs.hilt.android.gradle.plugin)
   }
 }
+
+subprojects {
+  tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+      if (project.findProperty("composeCompilerReports") == "true") {
+        freeCompilerArgs += listOf(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
+        )
+      }
+      if (project.findProperty("composeCompilerMetrics") == "true") {
+        freeCompilerArgs += listOf(
+          "-P",
+          "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
+        )
+      }
+    }
+  }
+}
