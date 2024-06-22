1a7e45d5a8c836e907783932f4c65ceacc0ba7f0
Shreyansh Lodha <slodha96@gmail.com>
2024-06-22 12:31:58 +0530

    misc: Code update
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- designsystem/src/main/java/dev/staticvar/designsystem/component/text/BreezeText.kt
+++ designsystem/src/main/java/dev/staticvar/designsystem/component/text/BreezeText.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -128,18 +129,20 @@ fun BreezeLabel(
 @Composable
 private fun BreezeTexts(modifier: Modifier = Modifier) {
 
-  BreezeTheme {
-    Column(
-      modifier = Modifier
-        .background(BreezeTheme.colorScheme.background)
-        .padding(BreezeTheme.dimens.medium),
-      verticalArrangement = Arrangement.spacedBy(BreezeTheme.dimens.large)
-    ) {
-      BreezeLargeText("Breeze Large")
-      BreezeTitleText("Breeze Title")
-      BreezeSubtitleText("Breeze Subtitle")
-      BreezeText("Breeze Default Text")
-      BreezeLabel("Breeze Label Text")
+  BreezeTheme() {
+    Surface {
+      Column(
+        modifier = Modifier
+          .background(BreezeTheme.colorScheme.background)
+          .padding(BreezeTheme.dimens.medium),
+        verticalArrangement = Arrangement.spacedBy(BreezeTheme.dimens.large)
+      ) {
+        BreezeLargeText("Breeze Large")
+        BreezeTitleText("Breeze Title")
+        BreezeSubtitleText("Breeze Subtitle")
+        BreezeText("Breeze Default Text")
+        BreezeLabel("Breeze Label Text")
+      }
     }
   }
 }
