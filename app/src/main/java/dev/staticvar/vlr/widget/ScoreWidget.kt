c0fa423111ca07b2849891714c065e109fd767b7
Shreyansh Lodha <slodha96@gmail.com>
2024-06-30 23:02:51 +0530

    update glance widget
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/staticvar/vlr/widget/ScoreWidget.kt
+++ app/src/main/java/dev/staticvar/vlr/widget/ScoreWidget.kt
@@ -6,39 +6,61 @@ import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.net.toUri
+import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.Image
+import androidx.glance.ImageProvider
 import androidx.glance.LocalContext
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.action.actionStartActivity
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.components.Scaffold
+import androidx.glance.appwidget.components.TitleBar
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
 import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.get
+import com.github.michaelbull.result.getOr
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 import dev.staticvar.vlr.MainActivity
+import dev.staticvar.vlr.R
 import dev.staticvar.vlr.data.VlrRepository
 import dev.staticvar.vlr.data.api.response.MatchPreviewInfo
 import dev.staticvar.vlr.ui.Destination
-import dev.staticvar.vlr.ui.theme.VLRTheme
 import dev.staticvar.vlr.ui.theme.WidgetTheme
 import dev.staticvar.vlr.utils.Constants
+import dev.staticvar.vlr.utils.Waiting
+import dev.staticvar.vlr.utils.onFail
+import dev.staticvar.vlr.utils.onPass
+import dev.staticvar.vlr.utils.onWaiting
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -55,53 +77,98 @@ class ScoreWidget : GlanceAppWidget() {
     var list by remember { mutableStateOf(listOf<MatchPreviewInfo>()) }
     val coroutine = rememberCoroutineScope()
     val context = LocalContext.current
-
-    LaunchedEffect(Unit) {
-      coroutine.launch(Dispatchers.IO) { list = vlrRepository.upcomingMatches().subList(0, 10) }
-    }
+    val state by vlrRepository.updateLatestMatches().collectAsState(initial = Ok(false))
+    val resultList by
+      vlrRepository.getMatchesFromDb().collectAsState(initial = Waiting<List<MatchPreviewInfo>>())
 
     WidgetTheme(context = context, darkTheme = context.isDarkThemeOn()) {
-      if (list.isEmpty())
-        WidgetUnableToUpdateUi()
-      else {
-        LazyColumn(
-          GlanceModifier.fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primaryContainer)
-            .cornerRadius(16.dp)
-        ) {
-          headerText()
-          items(list) {
-            Column(
+      Scaffold(
+        modifier = GlanceModifier.appWidgetBackground().fillMaxWidth(),
+        backgroundColor = GlanceTheme.colors.widgetBackground,
+        titleBar = {
+          TitleBar(
+            title = "Matches",
+            modifier = GlanceModifier.fillMaxWidth(),
+            startIcon = ImageProvider(resId = R.drawable.ic_launcher_foreground),
+          ) {
+            Image(
               modifier =
-                GlanceModifier.fillMaxWidth()
-                  .cornerRadius(16.dp)
-                  .padding(8.dp)
-                  .clickable(
-                    actionStartActivity(
-                      Intent(
-                        Intent.ACTION_VIEW,
-                        "${Constants.DEEP_LINK_BASEURL}${Destination.Match.Args.ID}=${it.id}".toUri(),
-                        context,
-                        MainActivity::class.java
-                      )
-                    )
-                  ),
-            ) {
-              Column(
-                modifier =
-                  GlanceModifier.fillMaxWidth()
-                    .cornerRadius(16.dp)
-                    .padding(4.dp)
-                    .background(VLRTheme.colorScheme.secondaryContainer),
-              ) {
-                WidgetTimeRow(status = it.status, time = it.time)
-                WidgetTeamUiRow(teamNameA = it.team1.name, teamNameB = it.team2.name)
-                WidgetScoreUiRow(teamScoreA = it.team1.score, teamScoreB = it.team2.score)
-              }
-            }
+                GlanceModifier.padding(8.dp).cornerRadius(100.dp).clickable {
+                  if (state.get() != true) coroutine.launch(Dispatchers.IO) {}
+                },
+              provider = ImageProvider(resId = R.drawable.rounded_refresh),
+              colorFilter = ColorFilter.tint(GlanceTheme.colors.onSurface),
+              contentDescription = "Refresh",
+            )
+          }
+        },
+      ) {
+        resultList
+          .onFail { WidgetUnableToUpdateUi() }
+          .onWaiting { WaitingUi() }
+          .onPass {
+            data?.let { MatchList(list = it, isUpdating = state.getOr(false)) }
+              ?: run { WidgetUnableToUpdateUi() }
+          }
+      }
+    }
+  }
+
+  @Composable
+  fun MatchList(
+    modifier: GlanceModifier = GlanceModifier,
+    list: List<MatchPreviewInfo>,
+    isUpdating: Boolean = false,
+  ) {
+    val context = LocalContext.current
+    LazyColumn(modifier = modifier.cornerRadius(16.dp).fillMaxWidth()) {
+      headerText(isUpdating = isUpdating)
+      items(list) {
+        Column(
+          modifier =
+            GlanceModifier.fillMaxWidth()
+              .cornerRadius(16.dp)
+              .padding(vertical = 4.dp)
+              .clickable(
+                actionStartActivity(
+                  Intent(
+                    Intent.ACTION_VIEW,
+                    "${Constants.DEEP_LINK_BASEURL}${Destination.Match.Args.ID}=${it.id}".toUri(),
+                    context,
+                    MainActivity::class.java,
+                  )
+                )
+              )
+        ) {
+          Column(
+            modifier =
+              GlanceModifier.fillMaxWidth()
+                .cornerRadius(16.dp)
+                .padding(4.dp)
+                .background(GlanceTheme.colors.primaryContainer)
+          ) {
+            WidgetTimeRow(status = it.status, time = it.time)
+            WidgetTeamUiRow(teamNameA = it.team1.name, teamNameB = it.team2.name)
+            WidgetScoreUiRow(teamScoreA = it.team1.score, teamScoreB = it.team2.score)
           }
         }
       }
+    }
+  }
+
+  @Composable
+  fun WaitingUi(modifier: GlanceModifier = GlanceModifier) {
+    Box(modifier = GlanceModifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+      Text(
+        text = "Updating...",
+        style =
+          TextStyle(
+            textAlign = TextAlign.Center,
+            color = ColorProvider(MaterialTheme.colorScheme.onPrimaryContainer),
+            fontSize = 12.sp,
+          ),
+        modifier = GlanceModifier.fillMaxWidth().padding(horizontal = 8.dp),
+      )
     }
   }
 
