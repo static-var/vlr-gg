91a4c133b2fb028a6abf21df416bdaf006cf05bc
Shreyansh Lodha <slodha96@gmail.com>
2025-01-21 06:33:46 +0530

    feat: Update VlrRepository constructor parameters
    Updates the `VlrRepository` constructor to accept `MatchFavDao`, `EventFavDao`, and `TeamFavDao` in addition to the previously accepted parameters.

--- app/src/main/java/dev/staticvar/vlr/di/StorageModule.kt
+++ app/src/main/java/dev/staticvar/vlr/di/StorageModule.kt
@@ -7,6 +7,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.staticvar.vlr.data.VlrRepository
+import dev.staticvar.vlr.data.dao.EventFavDao
+import dev.staticvar.vlr.data.dao.MatchFavDao
+import dev.staticvar.vlr.data.dao.TeamFavDao
 import dev.staticvar.vlr.data.dao.VlrDao
 import dev.staticvar.vlr.data.db.Migration_7_8
 import dev.staticvar.vlr.data.db.VlrDB
@@ -22,7 +25,9 @@ import kotlinx.serialization.json.Json
 @InstallIn(SingletonComponent::class)
 object StorageModule {
 
-  @Provides @Singleton fun typeConverter(json: Json) = VlrTypeConverter(json)
+  @Provides
+  @Singleton
+  fun typeConverter(json: Json) = VlrTypeConverter(json)
 
   @Provides
   @Singleton
@@ -33,17 +38,30 @@ object StorageModule {
       .addMigrations(Migration_7_8)
       .build()
 
-  @Provides @Singleton fun getVlrDao(db: VlrDB) = db.getVlrDao()
-  @Provides @Singleton fun getEventFavDao(db: VlrDB) = db.getEventFavDao()
-  @Provides @Singleton fun getMatchFavDao(db: VlrDB) = db.getMatchFavDao()
-  @Provides @Singleton fun getTeamFavDao(db: VlrDB) = db.getTeamFavDao()
+  @Provides
+  @Singleton
+  fun getVlrDao(db: VlrDB) = db.getVlrDao()
+  @Provides
+  @Singleton
+  fun getEventFavDao(db: VlrDB) = db.getEventFavDao()
+  @Provides
+  @Singleton
+  fun getMatchFavDao(db: VlrDB) = db.getMatchFavDao()
+  @Provides
+  @Singleton
+  fun getTeamFavDao(db: VlrDB) = db.getTeamFavDao()
 
+  @Suppress("LongParameterList")
   @Provides
   @Singleton
   fun getVlrRepository(
     vlrDao: VlrDao,
+    matchFavDao: MatchFavDao,
+    eventFavDao: EventFavDao,
+    teamFavDao: TeamFavDao,
     @Named("vlrClient") ktorHttpClient: HttpClient,
     @IoDispatcher ioDispatcher: CoroutineDispatcher,
-    json: Json
-  ) = VlrRepository(vlrDao, ktorHttpClient, ioDispatcher, json)
+    json: Json,
+  ) =
+    VlrRepository(vlrDao, matchFavDao, eventFavDao, teamFavDao, ktorHttpClient, ioDispatcher, json)
 }
