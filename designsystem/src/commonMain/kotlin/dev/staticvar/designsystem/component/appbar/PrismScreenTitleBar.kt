a32d851dfc347bec235b04140f684c674f77a6f8
Shreyansh Lodha <me@staticvar.dev>
2026-07-04 19:45:35 +0530

    fix(designsystem): remove title bar horizontal padding

--- designsystem/src/commonMain/kotlin/dev/staticvar/designsystem/component/appbar/PrismScreenTitleBar.kt
+++ designsystem/src/commonMain/kotlin/dev/staticvar/designsystem/component/appbar/PrismScreenTitleBar.kt
@@ -4,7 +4,6 @@
  */
 package dev.staticvar.designsystem.component.appbar
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,11 +21,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import dev.staticvar.designsystem.component.header.PrismHeader
 import dev.staticvar.designsystem.component.icon.PrismIcon
 import dev.staticvar.designsystem.component.icon.PrismIconStyle
 import dev.staticvar.designsystem.component.icon.PrismIconTint
-import dev.staticvar.designsystem.prism.Prism
 import dev.staticvar.designsystem.prism.icon.back.StairStepBack
 
 /**
@@ -53,7 +52,7 @@ public fun PrismScreenTitleBar(
   Row(
     modifier = modifier
       .height(style.containerHeight + systemBarHeight)
-      .padding(horizontal = style.horizontalPadding, vertical = style.verticalPadding)
+      .padding(horizontal = 0.dp, vertical = style.verticalPadding)
       .windowInsetsPadding(WindowInsets.statusBars),
     verticalAlignment = Alignment.CenterVertically,
   ) {
@@ -84,9 +83,7 @@ public fun PrismScreenTitleBar(
 }
 
 @Composable
-private fun PrismScreenTitleNavigationSlot(
-  onBackPress: (() -> Unit)?,
-) {
+private fun PrismScreenTitleNavigationSlot(onBackPress: (() -> Unit)?) {
   if (onBackPress != null) {
     PrismIcon(
       imageVector = StairStepBack,
@@ -96,7 +93,7 @@ private fun PrismScreenTitleNavigationSlot(
         onClick = onBackPress,
       ),
       tint = PrismIconTint.Inverted,
-      style = PrismIconStyle.Borderless
+      style = PrismIconStyle.Borderless,
     )
   }
 }
