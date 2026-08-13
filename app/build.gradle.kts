c9c655b6cdf47ac84f824c3b79bdde000754372f
Shreyansh Lodha <me@staticvar.dev>
2026-08-13 08:45:12 +0530

    chore(release): prepare v0.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ import java.util.Properties
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 val vlrApplicationId = "dev.staticvar.vlr"
-val vlrVersionCode = 69
-val vlrVersionName = "v0.6.3"
+val vlrVersionCode = 70
+val vlrVersionName = "v0.6.4"
 
 plugins {
   alias(libs.plugins.android.application)
