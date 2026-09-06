14e42783c0cd77fc250c4a695184d3a4262917d3
Shreyansh Lodha <me@staticvar.dev>
2026-09-06 21:12:01 +0530

    Distinguish Android debug installations

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -53,6 +53,10 @@ android {
   }
 
   buildTypes {
+    debug {
+      applicationIdSuffix = ".debug"
+      versionNameSuffix = "-debug"
+    }
     release {
       isMinifyEnabled = false
       proguardFiles(
